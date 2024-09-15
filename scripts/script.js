d9b76172b8d6666a75f0ad7a4b2a62f60c7deb91
zak <imzakbro@gmail.com>
2024-09-15 23:50:26 +0100

    ui

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,6 @@ const BODY = document.getElementsByTagName("body")[0];
 
 const INPT = document.getElementById("inpt");
 const BTN_1 = document.getElementById("decrypt");
-const BTN_2 = document.getElementById("decrypt_2");
 
 const PAGE =
   "U2FsdGVkX1+4SExk5j9WOX8a3jvTCRX4tLjoVPBKRx+3MshvJC9Pldh2WUySrwotVPWmAXRfhN9w80N+KILjh/bV9TFW/nz5dRWgi4JT/g6P6qAoR/9c6msqLuQtnV8H";
@@ -12,14 +11,8 @@ const PAGE =
 function decrypt_btn() {
   try {
     BODY.innerHTML = decrypt(PAGE, INPT.value);
+    set_cookie("decrypt_token", INPT.value);
     STYLE.remove();
-  } catch (error) {}
-}
-
-function decrypt_btn_2() {
-  try {
-    decrypt_btn();
-    //save the token
   } catch (error) {}
 }
 
@@ -27,7 +20,5 @@ BTN_1.addEventListener("click", function () {
   decrypt_btn();
 });
 
-BTN_2.addEventListener("click", function () {
-  decrypt_btn_2();
-});
-
+// get the token from the cookies
+INPT.value = get_cookie("decrypt_token");
